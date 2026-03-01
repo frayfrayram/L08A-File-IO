87bbbde5b0e9399bf41ad17555620643f4f7c8f6
Ridicolaz <nicolas.finocchio@live.com>
2026-03-01 14:56:48 -0700

    Completed part two, and added comments.

--- src/scoretrakker/ScoreTrakker.java
+++ src/scoretrakker/ScoreTrakker.java
@@ -7,42 +7,62 @@ import java.util.Collections;
 import java.util.Scanner;
 
 public class ScoreTrakker {
-	private ArrayList<Student> StudentList = new ArrayList<Student>();
-	
-	public void loadDataFile(String str) {
-		try {
-			FileReader reader = new FileReader(str);
-			Scanner in = new Scanner(reader);
-			while(in.hasNext()) {
-				String first = in.next();
-				String last = in.next();
-				String name = first + " " + last;
-				String strScore = in.next();
-				int score = Integer.parseInt(strScore);
-				StudentList.add(new Student(name, score));
-			}	
-			in.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
+    private final ArrayList<Student> studentList = new ArrayList<Student>();
+    private final String[] files = {"scores.txt", "badscore.txt", "nofile.txt"};
 
-	}
-	
-	public void printInOrder() {
-		Collections.sort(StudentList);
-		for(Student student : StudentList) {
-			System.out.println(student);
-		}
-	}
-	
-	public void processFiles() {
-		loadDataFile("scores.txt");
-		printInOrder();
-	}
-	
-	public static void main(String[] args) {
-		ScoreTrakker ScoreTrakker = new ScoreTrakker();
-		ScoreTrakker.processFiles();
-	}
+    /**
+     * Reads the file and parses the file, putting students into the array.
+     *
+     * @param str File to read.
+     * @throws FileNotFoundException Thrown if the fil is not found.
+     */
+    public void loadDataFile(String str) throws FileNotFoundException {
+        FileReader reader = new FileReader(str);
+        Scanner in = new Scanner(reader);
+        while (in.hasNext()) {
+            String first = in.next();
+            String last = in.next();
+            String name = first + " " + last;
+            String strScore = in.next();
+            try {
+                int score = Integer.parseInt(strScore);
+                studentList.add(new Student(name, score));
+            } catch (NumberFormatException e) {
+                System.out.println("Incorrect format for " + name + " not a valid score: " + strScore);
+            }
+
+        }
+        in.close();
+    }
+
+    /**
+     * Prints out the student list sorted by score
+     */
+    public void printInOrder() {
+        Collections.sort(studentList);
+        for (Student student : studentList) {
+            System.out.println(student);
+        }
+    }
+
+    /**
+     * Go through and read each file in the files list.
+     */
+    public void processFiles() {
+        for (String file : files) {
+            studentList.clear();
+            try {
+                loadDataFile(file);
+                printInOrder();
+            } catch (FileNotFoundException e) {
+                System.out.println("Can't open file: " + file);
+            }
+        }
+    }
+
+    static void main() {
+        ScoreTrakker ScoreTrakker = new ScoreTrakker();
+        ScoreTrakker.processFiles();
+    }
 }
 
--- src/scoretrakker/Student.java
+++ src/scoretrakker/Student.java
@@ -1,25 +1,33 @@
 package scoretrakker;
 
-public class Student implements Comparable<Object> {
-	private String name;
-	private int score;
-	
-	public Student(String name, int score) {
-		this.name = name;
-		this.score = score;
-	}
+/**
+ * Holds on to the information about each student (name and score)
+ */
+public class Student implements Comparable<Student> {
+    private final String name;
+    private final int score;
 
-	@Override
-	public int compareTo(Object o) {
-		return Integer.compare(this.score, score);
-	}
-	
-	@Override
-	public String toString() {
-		String str;
-		str = name + " " + score;
-		return str;
-		
-	}
-	
+    /**
+     * Make a new student to hold information about that student
+     *
+     * @param name  Name of the student.
+     * @param score Current score of the student.
+     */
+    public Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        String str;
+        str = name + " " + score;
+        return str;
+
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        return Integer.compare(score, o.score);
+    }
 }
